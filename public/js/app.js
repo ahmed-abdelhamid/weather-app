29a164c3fd228778b6ac1af9199b9f36fcd928a8
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2019-03-06 15:51:11 +0300

    Remove console log form client javascript file

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Client side javascript file running');
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
