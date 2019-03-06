fb37a6de3c978e32729ebe6d6d33b342c1a76c20
Ahmed Abdelhamid <slevin.glevra@gmail.com>
2019-03-06 15:46:40 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const app = express();
+
+// Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setu static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Ahmed Abdelhamid'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Ahmed Abdelhamid'
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    message: 'Welcome to help page',
+    title: 'Help',
+    name: 'Ahmed Abdelhamid'
+  });
+});
+
+app.get('/weather', ({ query }, res) => {
+  if (!query.address) {
+    return res.send({ error: 'Address must be provided' });
+  }
+
+  geocode(query.address, (error, { latitude, longitude, location } = {}) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecast) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({ forecast, location, address: query.address });
+    });
+  });
+});
+
+app.get('/products', (req, res) => {
+  if (!req.query.search) {
+    return res.send({ error: 'You must provide a search term.' });
+  }
+
+  res.send({ products: [] });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Help article not found',
+    name: 'Ahmed Abdelhamid'
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Page not found',
+    name: 'Ahmed Abdelhamid'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000.');
+});
